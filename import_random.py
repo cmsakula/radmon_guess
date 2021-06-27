c7ecd34b15d99772f95dda62454bdaae9f77a51d
Eric Tang <etang619@gmail.com>
2021-06-27 11:15:13 +0800

    and range and count trials

--- import_random.py
+++ import_random.py
@@ -1,8 +1,18 @@
 import random
 
-r = random.randint(1, 100)
+while True :
+	lower_number = int( input('Enter a lower number: '))
+	higher_number = int( input('Enter a higher number: '))
+	if lower_number < higher_number :
+		break
+	else :
+		print (lower_number, ' is not smaller than ', higher_number, '. Pls enter again')
+
+r = random.randint(lower_number, higher_number)
+number_of_guess = 0
 
 while True :
+	number_of_guess = number_of_guess + 1
 	my_guess = input('Guess the number? ')
 	my_guess = int(my_guess)
 	if r == my_guess :
@@ -11,4 +21,4 @@ while True :
 		print ('the number is smaller')
 	elif r > my_guess :
 		print ('the number is bigger')
-print ('you have guess it right, its number: ', my_guess)
+print ('you have guessed ', number_of_guess, ' times. ', ' The number is ', my_guess)
